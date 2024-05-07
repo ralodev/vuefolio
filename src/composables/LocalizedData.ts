bb609054d9cbcfe370f3803fec140825384e360a
Raúl <ralo.dev@pm.me>
2024-05-06 22:44:07 -0600

    Add comment in LocalizedData composable

--- src/composables/LocalizedData.ts
+++ src/composables/LocalizedData.ts
@@ -1,6 +1,11 @@
 import { useI18n } from 'vue-i18n'
 import { ref, watch } from 'vue'
 
+/**
+ * Returns an array of <T> objects from i18n files, whenever the locale is updated, the objects are fetched again in the current language
+ * @param dataKey the name or path of the objects in i18n messages
+ * @returns a reactive array of objects
+ */
 export function useLocalizedData<T>(dataKey: string) {
   const { locale, tm } = useI18n()
   const data = ref<T>()
